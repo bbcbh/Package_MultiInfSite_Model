d75ead85eafff817359d4d60a7439df9cebfa95a
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2020-02-27 16:30:40 +1100

    Addition support for vaccine valid time, infection duration adjustment and symptom removal under SiteSpecificVaccination

--- src/infection/vaccination/SiteSpecificVaccination.java
+++ src/infection/vaccination/SiteSpecificVaccination.java
@@ -9,69 +9,45 @@ import person.AbstractIndividualInterface;
  * @author Ben Hui
  */
 public class SiteSpecificVaccination extends AbstractVaccination {
-    
+
     // EFFECT_INDEX_PROPORTION_VACC_COVERAGE_SETTING
     // If >0, this will represents the probability of vaccination as person enter population
     // If <0, this will represents the probability of vaccination through screening
     public static final int EFFECT_INDEX_PROPORTION_VACC_COVERAGE_SETTING = 0;
 
     // For infected
-    public static final int EFFECT_INDEX_TRANMISSION_EFFICACY_G = EFFECT_INDEX_PROPORTION_VACC_COVERAGE_SETTING+1;
+    public static final int EFFECT_INDEX_TRANMISSION_EFFICACY_G = EFFECT_INDEX_PROPORTION_VACC_COVERAGE_SETTING + 1;
     public static final int EFFECT_INDEX_TRANMISSION_EFFICACY_A = EFFECT_INDEX_TRANMISSION_EFFICACY_G + 1;
     public static final int EFFECT_INDEX_TRANMISSION_EFFICACY_R = EFFECT_INDEX_TRANMISSION_EFFICACY_A + 1;
 
     // For uninfected 
     public static final int EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_G = EFFECT_INDEX_TRANMISSION_EFFICACY_R + 1;
     public static final int EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_A = EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_G + 1;
-    public static final int EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_R = EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_A + 1;                
-    // Optional     
-    public static final int OPTIONAL_EFFECT_VACCINE_DURATION = EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_R + 1;
-    
-    
-   
-    
+    public static final int EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_R = EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_A + 1;
+    // Optional. -1 if not used
+    // Vaccine duration
+    public static final int OPTIONAL_EFFECT_VACCINE_DURATION_DEFAULT = EFFECT_INDEX_SUSCEPTIBLE_EFFICACY_R + 1;
+    // Vaccine infection duration adjustment
+    public static final int OPTIONAL_EFFECT_ADJ_INF_DUR_DEFAULT = OPTIONAL_EFFECT_VACCINE_DURATION_DEFAULT + 1;
+    // Vaccine symptom removal
+    public static final int OPTIONAL_EFFECT_REMOVE_SYM_RATE_DEFAULT = OPTIONAL_EFFECT_ADJ_INF_DUR_DEFAULT + 1;
+    public static final int OPTIONAL_EFFECT_REMOVE_SYM_STATE_DEFAULT = OPTIONAL_EFFECT_REMOVE_SYM_RATE_DEFAULT + 1;
+    public static final int OPTIONAL_EFFECT_REMOVE_SYM_INF_DUR_DEFAULT_MEDIAN = OPTIONAL_EFFECT_REMOVE_SYM_STATE_DEFAULT + 1;
+    public static final int OPTIONAL_EFFECT_REMOVE_SYM_INF_DUR_DEFAULT_SD = OPTIONAL_EFFECT_REMOVE_SYM_INF_DUR_DEFAULT_MEDIAN + 1;
+
     public SiteSpecificVaccination(double[] parameters) {
-        super(parameters);                                
+        super(parameters);
     }
 
-    
-
-    @Override
-    public int[] vaccinatePerson(AbstractIndividualInterface person) {
-        int[] res = super.vaccinatePerson(person);                
-        // Include possible immedate effect for vaccination (e.g. reduce duration, removal of syptoms etc)                                
-
-        return res;
+    public SiteSpecificVaccination(double[] parameters, int[] validTime) {
+        super(parameters, validTime);
     }
-    
-    
 
     @Override
     public double[] vaccineImpact(AbstractIndividualInterface person, Object params) {
-        // For now, params is not used 
-        int[] vaccRecord = getVaccinationRecord().get(person.getId());
-
-        if (vaccRecord == null) {
-            return null;
-        } else {
-            
-            /*
-            if(getParameters().length > OPTIONAL_EFFECT_VACCINE_DURATION){
-                double vaccDur = getParameters()[OPTIONAL_EFFECT_VACCINE_DURATION];
-                if(person.getAge() - vaccRecord[vaccRecord.length-1] > vaccDur){
-                    return null;
-                }                                
-            }
-            */
-            
-            
-            double[] res = new double[getParameters().length];
-            Arrays.fill(res, 1);
-            // No waning
-            System.arraycopy(getParameters(), 0, res, 0, res.length);
-            return res;
-        }
-
+        double[] res = new double[getParameters().length];
+        System.arraycopy(getParameters(), 0, res, 0, res.length);
+        return res;
     }
 
 }
